dcdeaa87ffc5f3956436c61fbf23ff8803f5c76f
Leonie <leonie.reif@gatech.edu>
2016-09-24 09:45:46 -0400

    didn't update correctly

--- facebookdata.js
+++ facebookdata.js
@@ -1,17 +1,16 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1179786725394019',
+    xfbml      : true,
+    version    : 'v2.7'
+  });
+};
 
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1179786725394019',
-      xfbml      : true,
-      version    : 'v2.7'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
