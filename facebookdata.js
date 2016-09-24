f68bb0328c573ae5135e54c6d606cf42cc121fc2
leoniereif <leonie.reif@gatech.edu>
2016-09-24 11:32:22 -0400

    new branch and file for sentimentanalysis

--- facebookdata.js
+++ facebookdata.js
@@ -26,6 +26,7 @@ window.fbAsyncInit = function() {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
+        var accessToken = response.authResponse.accessToken;
       testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
