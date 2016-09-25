1df86ced8307a873fdba8a56f4a870aaa0a63f5e
AlanisDavid <david.alanis.1997@gmail.com>
2016-09-25 02:10:07 -0400

    stuff

--- facebookdata.js
+++ facebookdata.js
@@ -28,7 +28,7 @@ window.fbAsyncInit = function() {
       // Logged into your app and Facebook.
         var accessToken = response.authResponse.accessToken;
         console.log(accessToken);
-      testAPI();
+        showplacesdata();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
@@ -70,14 +70,3 @@ window.fbAsyncInit = function() {
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
---
+++ stylesheets/googleMapsStuff.js
@@ -0,0 +1,7 @@
+/**
+ * Created by David on 9/24/16.
+ */
+function getMarkers(){
+    console.log(taggedPlaces)
+    return taggedPlaces
+}
