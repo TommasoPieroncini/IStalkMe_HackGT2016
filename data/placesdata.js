6b35b1367b0fbbd7b410e5efdadfc5c289a957d9
AlanisDavid <david.alanis.1997@gmail.com>
2016-09-25 04:41:46 -0400

    adsafasdfadsf

--- data/placesdata.js
+++ data/placesdata.js
@@ -90,7 +90,8 @@ function predominantCityPerYear(yearToCityMap, years) {
 function predominantCity(citieslist, year) {
     var cities = {}
     for (var i = 0; i < citieslist.length; i++) {
-        if (cities[i].place.location.city) {
+
+        if (cities[i].place && cities[i].place.location && cities[i].place.location.city) {
             var city = citieslist[i].place.location.city
             if (city in cities) {
                 cities[city] = cities[city] + 1
